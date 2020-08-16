b2d276cac7d20930398fcb7ee548804118b92aa4
freeman <1099983219@qq.com>
2020-08-16 12:50:05 +0800

    更新0816

---
+++ docs/.vuepress/components/BasicsList.vue
@@ -0,0 +1,18 @@
+<template>
+  <home-layout title="基础知识列表:"></home-layout>
+</template>
+<script >
+import homeLayout from "./layout/homeLayout";
+export default {
+  components: {
+    homeLayout
+  },
+  data() {
+    return {}
+  },
+  methods: {
+    
+  }, 
+  mounted() {}
+}
+</script>
